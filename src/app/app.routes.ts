679cac8a8bd06815fca6636265120ac41ddf74a6
ZarateRamiro <ramirozarate58@gmail.com>
2025-12-01 21:57:37 -0300

    chore: initial portfolio upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import path from 'path';
 import { MisDatosComponent } from './features/mis-datos/mis-datos.component';
 import { VistaProyectosComponent } from './features/vista-proyectos/vista-proyectos.component';
+import { InicioComponent } from './features/inicio/inicio.component';
 
 export const routes: Routes = [
     {path:'', component:VistaProyectosComponent},
-    {path:'datos',component:MisDatosComponent}
+    {path:'datos',component:MisDatosComponent},
+    {path:'proyectos',component:VistaProyectosComponent},
+    {path:'inicio',component:InicioComponent}
     
 ];
--- src/app/features/vista-proyectos/vista-proyectos.component.ts
+++ src/app/features/vista-proyectos/vista-proyectos.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { proyectoService } from '../../services/proyecto-service.service';
 
 @Component({
   selector: 'app-vista-proyectos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './vista-proyectos.component.html',
-  styleUrl: './vista-proyectos.component.css'
+  styleUrls: ['./vista-proyectos.component.css']
 })
-export class VistaProyectosComponent {
+export class VistaProyectosComponent implements OnInit {
+  vistaProyectos: any[] = [];
+  cargando: boolean = false;
+  error: string = "";
 
+  constructor(private proyectoService: proyectoService) {}
+
+  ngOnInit() {
+    this.cargando = true;
+
+    this.proyectoService.obtenerProyectos().subscribe({
+      next: (data) => {
+        console.log("Proyectos obtenidos correctamente");
+        console.log(JSON.stringify(data));
+
+        this.vistaProyectos = data;
+        this.cargando = false;
+      },
+      error: (err) => {
+        console.error(err);
+        this.error = 'No se pudieron cargar tus datos.';
+        this.cargando = false;
+      }
+    });
+  }
 }
